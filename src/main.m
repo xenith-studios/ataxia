8352e9c04d1ce69862b75a1be078c78ba16436e2
Justin Rocha <xenith@xenith.org>
2009-12-15 09:15:32 -0800

    More base structure, sample code, and sample documentation.

--- src/main.m
+++ src/main.m
@@ -1,10 +1,16 @@
+/*
+ * Ataxia Engine Project
+ * Copyright (C) 2009  Xenith Studios
+ * See COPYING for license details
+ * http://ataxia.xenith.org/
+ *
+ */
+
 #import <Foundation/Foundation.h>
 
 int main (int argc, const char * argv[]) {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
 
-    // insert code here...
-    NSLog(@"Hello, World!");
     [pool drain];
     return 0;
 }
