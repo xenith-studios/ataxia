ea631e0a89cdb3b9189eda011850cd0bff5b17ae
Justin Rocha <xenith@xenith.org>
2011-03-18 16:58:29 -0700

    Successful chat server is successful.

--- src/player.go
+++ src/player.go
@@ -1,38 +1,118 @@
+/*
+    Player structures and functions
+*/
 package main
 
 import (
     "net"
-    "time"
+//    "time"
     "container/list"
+    "os"
+    "log"
+    "fmt"
+    "bytes"
+    "bufio"
 )
 
+// The Connection structure wraps all the lower networking details for each connected player
 type Connection struct {
-    inBuffer []uint8
-    outBuffer []uint8
-    Socket net.Conn
-    Status string
-    Account Account
-    Player Player
+    socket net.Conn
+    reader *bufio.Reader
+    State string
 }
 
+// Account
 type Account struct {
     Email string
-    password string
+    Password string
+    Name string
     Characters *list.List
 }
 
+// Player
 type Player struct {
+    account Account
+    connection Connection
+    In chan string
+    Out chan string
+    inBuffer []byte
+    outBuffer []byte
 }
 
-var PlayerList *list.List
 
-func init() {
-    PlayerList = list.New()
+func (account *Account) Setup() {
+//    account.Write([]byte("Hello, welcome to Ataxia. What is your account name?\n"))
+//    buf, _ := account.Read()
+//    account.Write([]byte(fmt.Sprintf("Hello %s, how are you today?\n", buf)))
 }
 
-func (conn *Connection) Run() {
-    for {
-        conn.Socket.Write([]uint8("Hello!\n"))
-        time.Sleep(60*60*60*60*60)
+
+
+// Player factory
+func NewPlayer(conn net.Conn) (player *Player) {
+    player = new(Player)
+    player.connection.socket = conn
+    player.connection.reader = bufio.NewReader(conn)
+    return player
+}
+
+func (player *Player) Run() (err os.Error) {
+    var data string
+    
+    // Setup the player here.
+    player.Write("Hello, welcome to Ataxia. What is your account name?\n")
+    if data, err = player.Read(); err != nil {
+        return err
     }
+    player.Write(fmt.Sprintf("Hello %s.\n", data))
+    player.account.Name = data
+    player.Write("\n\n> ")
+    //player.connection.socket.SetTimeout(3600)
+    for {
+        if player.connection.socket == nil {
+            break
+        }
+        
+        if data, err = player.Read(); err != nil {
+            log.Println(err)
+            player.Close()
+            break
+        }
+        
+        if len(data) == 0 {
+            continue
+        }
+        
+        mainServer.SendToAll(fmt.Sprintf("<%s> %s", player.account.Name, data))
+    }
+    
+    return
+}
+
+func (player *Player) Close() {
+    if (player.connection.socket != nil) {
+        player.connection.socket.Close()
+        player.connection.socket = nil
+        player.connection.reader = nil
+    }
+}
+
+func (player *Player) Write(buf string) (n int, err os.Error) {
+    n, err = player.connection.socket.Write([]byte(buf))
+    return n, err
+}
+
+func (player *Player) Read() (data string, err os.Error) {
+    buf := make([]byte, 1024)
+    if player.connection.reader == nil {
+        return
+    }
+
+    if buf, err = player.connection.reader.ReadBytes('\n'); err != nil {
+        return
+    }
+    //buf = buf[0:len(buf)-1]
+    buf = bytes.TrimSpace(buf)
+    data = string(buf)
+    return
 }
--- src/server.go
+++ src/server.go
@@ -1,52 +1,72 @@
 /*
-    Networking functions
+    Server functions
 */
 package main
 
 import (
     "log"
     "net"
+    "os"
+    "fmt"
+    "container/list"
     "ataxia/settings"
 )
 
 type Server struct {
     Socket *net.TCPListener
+    PlayerList *list.List
 }
 
-var mainServer *Server
+var mainServer Server
 
 func InitializeNetwork() bool {
-    if mainServer != nil {
+    if mainServer.Socket != nil {
         return true
     }
-    mainServer = new(Server)
     listener, err := net.ListenTCP("tcp", &net.TCPAddr{net.ParseIP(""), settings.MainPort})
     if err != nil {
         log.Fatalln("Failed to create server:", err)
         return false
     }
     mainServer.Socket = listener
+    mainServer.PlayerList = new(list.List)
     return true
 }
 
-func (server *Server) Shutdown() {
+func (server Server) Shutdown() {
     if server.Socket != nil {
+        for e := server.PlayerList.Front(); e != nil; e = e.Next() {
+            player := e.Value.(*Player)
+            player.Close()
+        }
         server.Socket.Close()
     }
 }
 
-func (server *Server) Listen() {
+func (server Server) Listen() {
     for {
         conn, err := server.Socket.Accept()
         if err != nil {
             log.Println("Failed to accept new connection")
         } else {
             log.Println("Accepted a new connection:", conn.RemoteAddr().String())
-            connection := new(Connection)
-            connection.Socket = conn
-            connection.Status = "connecting"
-            PlayerList.PushBack(connection)
-            go connection.Run()
+            player := NewPlayer(conn)
+            server.PlayerList.PushBack(player)
+            go player.Run()
         }
     }
+}
+
+
+func (server Server) SendToAll(buf string) {
+    for e := server.PlayerList.Front(); e != nil; e = e.Next() {
+        player := e.Value.(*Player)
+        player.Write(fmt.Sprintf("\n%s\n", buf))
+        player.Write("> ")
+    }
+}
+
+
+func (server Server) Write(buf []byte) (n int, err os.Error) {
+    return 0, os.EOF
 }
